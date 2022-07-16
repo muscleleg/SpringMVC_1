f3853498ccdcd14d0d129b96576b34b39c6d5dc1
jaejun <aamoof@gmail.com>
2022-07-16 11:23:43 +0900

    싱글톤 멤버변수는 어처피 고정적인 객체인데 굳이 그 안에 변수까지 왜 static을 붙이는지가 궁금했음 결과적으로는 static을 안 붙여도 된다임. 하지만 붙이는 것이 맞다. 그 이유는 나는 개인적으로 이렇다고 생각함. 멤버변수에 static을 붙여주면 하나의 데이터를 공유해서 쓰는게 아니라 각각 객체를 만들고 데이터를 쓰는 것을 원천적으로 막아버리기 때문에 static을 붙여줘야함 예를들어 클래스파일 로드 과정에서 new하여 실제 코드에서 getInstance로 접근하는 경우는 static을 멤버변수에 붙이던 안붙이던 의미가 없음 하지만 사용하는 코드에서 new로 생성하여 접근하면 결과적으로 MemberRepository는 여러개가 생성되여 싱글톤이 아닐거임 하지만 멤버변수를 static으로 고정했기때문에 memberRepository는 멤버변수를 공유하게 될것이고 저장소 역할은 할 수 있음. 하지만 static을 안 붙이면 그렇지 못함

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -14,11 +14,14 @@ public class MemberRepository {
     //https://readystory.tistory.com/116해당 링크를 보면 왜 자기 자신에서 new를 하는지 알 수 있음
     //클래스 로딩 단계에서 new를 해서 자기 자신을(MemberRespository) 생성하고 전역화시킴
     //남이 못생성하게 private로 생성자를 막아버리는거임
-    //그리고 store나 sequence를 get이나 set이 아닌 다른걸로 못바꾸게 private로 잠궈버리고 어디서든 쓸 수 있게 전역화함(싱글톤이기 때문에 어디서든 사용할 수 있어야함
-    //static을 store나 sequence에 선언을 안해주면 memberRepository.store나 memberRepository.sequence로 사용해야하는데
-    //static을 넣으면 MemberRepository클래스만 사용하려는 class에 import해주면 sequence= 1L이런식으로 사용 가능한것 같음, 하지만 MemberRepository.java에서는
-    //싱글톤을 구현하기위해 private로 잠궈버렸기때문에 그렇게 사용 못하게 막음, sequence와 store를 보호하기위해, memberRepository가 제공하는 메서드를 통해
-    //수정하는것을 강제하기위해 잠금
+   // 그리고 store와 sequence를 static한 이유는 여러곳에서 한 데이터를 사용하게 하려는것도 있지만
+    //어처피 싱글통으로 생성하기때문에 여기서는 그 의미가 사라짐
+    //왜냐하면 이미 MemberRepository라는 객체가 메모리에 고정되어있고 그 안에 변수들을 바꾸기 때문에
+    //static과 같은 결과를 내기 때문임
+    //하지만 왜 static을 붙였냐면 나중에 이 MemberRepository객체를 private로 생성자 막아버린거
+    //없애고 새로운 객체인 new로 생성하게 된다면 static을 사용안 할 시 각각 객체마다 데이터를 갖게 되버림
+    //일종의 이중 보안인것 같음, 멤버변수에 static을 넣으면 어쨋든 중간 코드가 이상해도 static을 통해서
+    //하나의 데이터만 접근하게 되니 결과적으로 각각의 데이터가 생성될 일은 없을 거임
     private static final MemberRepository instance = new MemberRepository();
 
     public static MemberRepository getInstance(){
